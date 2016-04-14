de21a070707e3520d68c8bda698b784d244eb9fb
javiersantibanez <javier.santibanez.93@gmail.com>
2016-04-13 23:18:18 -0300

    Added files via upload

---
+++ Test v2/src/test/v2/TestV2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.v2;
+
+/**
+ *
+ * @author Javier Santibañez
+ */
+public class TestV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu o = new menu();
+        o.show(true);
+    }
+    
+}
